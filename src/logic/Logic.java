d39733665ee8c208d411511dad8c5ab9fee949d3
buidohiep <buidohiep@gmail.com>
2015-10-08 05:27:46 +0800

    implement basic logic and parser

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -1,40 +1,76 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 import models.*;
-import parser.MainParser;
+import parser.Parser;
 
 public class Logic {
-	public Vector<Task> processCommand(String input) {
-		ArrayList<String> cmdContents = MainParser.ParseCommand(input);
 
-		switch (cmdContents.get(0)) {
-		case "add":
-			return addTask(cmdContents);
-		default:
-			return new Vector<Task>();
+	public static List<Task> tasks;
+	
+	public static void init() {
+	}
+	
+	public static Vector<Task> processCommand(String command) {
+		String commandType = Parser.getFirstWord(command);
+		command = Parser.removeFirstWord(command);
+		switch (commandType) {
+			case "add": return addTask(command); 
+			case "update": return updateTask(command);
+			case "display": return display(command);
+			case "delete": return deleteTask(command);
+			case "search": break;
+			default: break;
 		}
+		return null;
 	}
-
-	private Vector<Task> addTask(ArrayList<String> cmdContents) {
-		switch (cmdContents.get(1)) {
-		case "deadline":
-			break;
-		case "event":
-			Event evt = new Event(cmdContents.get(4), cmdContents.get(2), cmdContents.get(3), cmdContents.get(5), false);
-			//Storage.addTask(evt);
-			break;
-		default:
-
+	
+	private static Vector<Task> addTask(String command) {
+		Vector<Task> tasks = new Vector<>(); 
+		tasks.addElement(Parser.getTask(command));
+		return tasks;
+	}
+	
+	private static Vector<Task> updateTask(String command) {
+		Vector<Task> tasks = new Vector<>();
+		int id = Parser.getTaskId(command);
+		String desc = Parser.removeFirstWord(command);
+		Task task = findTaskById(id);
+		if (task == null) {
+			return tasks;
 		}
-		return new Vector<Task>();
+		task.setTaskDesc(desc);
+		tasks.addElement(task);
+		return tasks;
 	}
-
-	private Vector<Task> searchTask(ArrayList<String> cmdContents) {
-
+	
+	private static Vector<Task> display(String command) {
+		return new Vector<Task>(tasks);
 	}
-
-
+	
+	private static Vector<Task> deleteTask(String command) {
+		List<Integer> deletedIds = Parser.getTaskIds(command);
+		for (int i = 0; i < tasks.size(); i++) if (deletedIds.contains(tasks.get(i).getTaskID())){
+			tasks.remove(i);
+		}
+		return new Vector<Task>(tasks); 
+	}
+	
+	private static Vector<Task> search(String command) {
+		Vector<Task> matchedTasks = new Vector<>();
+		for (Task task: tasks) if (task.getTaskDesc().indexOf(command) != -1) {
+			matchedTasks.addElement(task);
+		}
+		return matchedTasks;
+	}
+	
+	private static Task findTaskById(int id) {
+		for (Task task: tasks) {
+			if (task.getTaskID() == id) return task;
+		}
+		return null;
+	}
 }
