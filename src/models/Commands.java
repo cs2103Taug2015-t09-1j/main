edc9d60bca54518cd0e5e4403cd71a1200e14d92
dyanm <daltonyan@gmail.com>
2015-10-14 04:28:26 +0800

    Updated Commands to include DISPLAY enum

--- src/models/Commands.java
+++ src/models/Commands.java
@@ -9,10 +9,10 @@ package models;
  */
 public class Commands {
 	public static enum COMMAND_TYPE {
-		ADD, DELETE, UPDATE, SEARCH, UNDO, INVALID
+		ADD, DELETE, UPDATE, SEARCH, DISPLAY, UNDO, INVALID
 	};
 
-	public static enum ADD_TYPE {
+	public static enum TASK_TYPE {
 		SINGLE_DATE_EVENT, DOUBLE_DATE_EVENT, DEADLINE_TASK, FLOATING_TASK, INVALID
 	};
 }
