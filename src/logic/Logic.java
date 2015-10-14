dff9eb6fa9fc1feb884d6c97ee02b1b9cf331e46
dyanm <daltonyan@gmail.com>
2015-10-15 01:38:38 +0800

    Updated the Logic class to enable the delete function

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -22,7 +22,7 @@ public class Logic {
 	private static Logic logic = null;
 	private static final MainParser parser = MainParser.getInstance();
 
-	private Logic() { }
+	private Logic() {}
 
 	public static Logic getInstance() {
 		if (logic == null) {
@@ -35,22 +35,16 @@ public class Logic {
 		switch (parser.determineCommandType(input)) {
 		case ADD:
 			return add(input);
-			//break;
 		case DISPLAY:
 			//display(input);
-			break;
 		case SEARCH:
 			//search(input);
-			break;
 		case UPDATE:
 			//update(input);
-			break;
 		case DELETE:
-			//delete(input);
-			break;
+			return delete(input);
 		case UNDO:
 			//undo(input);
-			break;
 		default:
 		}
 		return null;
@@ -65,15 +59,15 @@ public class Logic {
 	}
 
 	public ArrayList<Event> getAllEvents() {
-		return (ArrayList)Storage.getAllTask(Commands.TASK_TYPE.EVENT);
+		return (Storage.getAllTask(Commands.TASK_TYPE.EVENT).size() != 0) ? (ArrayList)Storage.getAllTask(Commands.TASK_TYPE.EVENT) : new ArrayList();
 	}
 
 	public ArrayList<FloatingTask> getAllFloatingTasks() {
-		return (ArrayList)Storage.getAllTask(Commands.TASK_TYPE.FLOATING_TASK);
+		return (Storage.getAllTask(Commands.TASK_TYPE.FLOATING_TASK).size() != 0) ? (ArrayList)Storage.getAllTask(Commands.TASK_TYPE.FLOATING_TASK) : new ArrayList();
 	}
 
 	public ArrayList<DeadlineTask> getAllDeadlineTasks() {
-		return (ArrayList)Storage.getAllTask(Commands.TASK_TYPE.DEADLINE_TASK);
+		return (Storage.getAllTask(Commands.TASK_TYPE.DEADLINE_TASK).size() != 0) ? (ArrayList)Storage.getAllTask(Commands.TASK_TYPE.DEADLINE_TASK) : new ArrayList();
 	}
 
 	private String add(String input) {
@@ -119,19 +113,26 @@ public class Logic {
 		return "Add command has failed.";
 	}
 
-	private void delete(String input) {
+	private String delete(String input) {
 		// For Debugging
 		ParsedObject obj = parser.getDeleteParsedObject(input);
 		System.out.println(obj.getCommandType());
 		System.out.println(obj.getTaskType());
 		ArrayList<Integer> v = obj.getObjects();
+		String statusMsg = "<html>Tasks IDs: <b>";
 		for (int i = 0; i < v.size(); i++) {
+			Storage.delete(v.get(i));
+			statusMsg += v.get(i);
 			System.out.print(v.get(i));
 			if (i < v.size()-1) {
+				statusMsg += ", ";
 				System.out.print("|");
 			}
 		}
 		System.out.println();
+		statusMsg += "</b> have been deleted successfully.<br/></html>";
+		return statusMsg;
+
 		// return Storage.delete(parser.getDeleteParsedObject(input));
 	}
 
@@ -166,97 +167,4 @@ public class Logic {
 			System.out.println();
 			// return Storage.search(parser.getSearchParsedObject(input));
 	}
-
-	/*
-	// need to be call when the app start
-	public static void init() {
-		tasks = Storage.getAllTask();
-		List<Task> _tasks = tasks;
-		int maxId = 0;
-		for (Task task: tasks) {
-			maxId = Math.max(maxId, task.getTaskID());
-		}
-		Task.nextId.set(maxId);
-	}
-
-	public static void processCommand(String command, MainGUI uiRef) {
-		Vector<Task> data = new Vector<>();
-		String message = null;
-
-		String commandType = Parser.getFirstWord(command);
-		command = Parser.removeFirstWord(command);
-		switch (commandType) {
-			case "add": data = addTask(command); break;
-			case "update": data = updateTask(command); break;
-			case "display": data = display(command); break;
-			case "delete": data = deleteTask(command); break;
-			case "search": data = search(command); break;
-			case "done": data = markTask(command, true); break;
-			case "undone": data = markTask(command, false); break;
-			default: message = "Incorrect format!";
-		}
-
-		Storage.saveAllTask(tasks);
-
-		if (message == null) {
-			uiRef.updateTables(data, "All");
-		}
-	}
-
-	private static Vector<Task> addTask(String command) {
-		Vector<Task> result = new Vector<>();
-		Task newTask = Parser.getTask(command);
-		result.addElement(newTask);
-		tasks.add(newTask);
-		System.out.println(tasks.size());
-		return result;
-	}
-
-	private static Vector<Task> updateTask(String command) {
-		Vector<Task> tasks = new Vector<>();
-		int id = Parser.getTaskId(command);
-		String desc = Parser.removeFirstWord(command);
-		Task task = findTaskById(id);
-		if (task == null) {
-			return tasks;
-		}
-		task.setTaskDesc(desc);
-		tasks.addElement(task);
-		return tasks;
-	}
-
-	private static Vector<Task> display(String command) {
-		return new Vector<Task>(tasks);
-	}
-
-	private static Vector<Task> deleteTask(String command) {
-		List<Integer> deletedIds = Parser.getTaskIds(command);
-		for (int i = 0; i < tasks.size(); i++) if (deletedIds.contains(tasks.get(i).getTaskID())){
-			tasks.remove(i);
-		}
-		return new Vector<Task>(tasks);
-	}
-
-	private static Vector<Task> search(String command) {
-		Vector<Task> matchedTasks = new Vector<>();
-		for (Task task: tasks) if (task.getTaskDesc().indexOf(command) != -1) {
-			matchedTasks.addElement(task);
-		}
-		return matchedTasks;
-	}
-
-	private static Vector<Task> markTask(String command, boolean isDone)  {
-		List<Integer> markIds = Parser.getTaskIds(command);
-		for (Task task: tasks) if (markIds.contains(task.getTaskID())){
-			task.setDone(isDone);
-		}
-		return new Vector<Task>(tasks);
-	}
-
-	private static Task findTaskById(int id) {
-		for (Task task: tasks) {
-			if (task.getTaskID() == id) return task;
-		}
-		return null;
-	}*/
 }
