664eb4c81386fcbffc191ca801aa1cbd8e986085
dyanm <daltonyan@gmail.com>
2015-10-17 19:58:42 +0800

    Modified some of the regex patterns to handle some special input cases

--- src/parser/MainParser.java
+++ src/parser/MainParser.java
@@ -3,12 +3,16 @@ package parser;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.ArrayList;
 import java.util.Date;
 
 import org.ocpsoft.prettytime.nlp.*;
 import org.ocpsoft.prettytime.nlp.parse.DateGroup;
+import org.ocpsoft.prettytime.shade.edu.emory.mathcs.backport.java.util.Arrays;
 
+import logic.Logic;
 import models.DeadlineTask;
 import models.Event;
 import models.FloatingTask;
@@ -19,6 +23,7 @@ import models.Commands.*;
 public class MainParser {
 	private static MainParser parser = null;
 	private final PrettyTimeParser ptParser = new PrettyTimeParser();
+	private final Logger logger = Logger.getLogger(MainParser.class.getName());
 	private String[] updateCmdList = {"update", "/u", "edit", "/e", "modify", "/m"};
 	private String[] deleteCmdList = {"delete", "del", "/d", "remove", "rm", "/r"};
 	private String[] searchCmdList = {"search", "/s", "find", "/f"};
@@ -34,16 +39,20 @@ public class MainParser {
 	}
 
 	public COMMAND_TYPE determineCommandType(String input) {
-		if (isCommand(input, updateCmdList)) {
-			return COMMAND_TYPE.UPDATE;
-		} else if (isCommand(input, deleteCmdList)) {
-			return COMMAND_TYPE.DELETE;
-		} else if (isCommand(input, searchCmdList)) {
-			return COMMAND_TYPE.SEARCH;
-		} else if (isCommand(input, displayCmdList)) {
-			return COMMAND_TYPE.DISPLAY;
+		if (input.trim().isEmpty()) {
+			return COMMAND_TYPE.INVALID;
 		} else {
-			return COMMAND_TYPE.ADD;
+			if (isCommand(input, updateCmdList)) {
+				return COMMAND_TYPE.UPDATE;
+			} else if (isCommand(input, deleteCmdList)) {
+				return COMMAND_TYPE.DELETE;
+			} else if (isCommand(input, searchCmdList)) {
+				return COMMAND_TYPE.SEARCH;
+			} else if (isCommand(input, displayCmdList)) {
+				return COMMAND_TYPE.DISPLAY;
+			} else {
+				return COMMAND_TYPE.ADD;
+			}
 		}
 	}
 
@@ -81,8 +90,8 @@ public class MainParser {
 		String date = temp.get(0).getText();
 		input = input.toLowerCase().replaceAll("\\++((due by)|due|by|before|from|on|at)\\s*" + date, "");
 		input = input.replaceAll("\\s*" + date + "\\s*", "");
-		input = input.replaceAll("\\s+((due by)|due|by|from|on|at)\\s*$", "");
-		input = input.replaceAll("^\\s*((due by)|due|by|from|on|at)", "");
+		input = input.replaceAll("\\s+((due by)|due|by|from|on|at)\\s*((due by)|due|by|from|on|at)*\\s*$", "");
+		input = input.replaceAll("^\\s*((due by)|due|by|from|on|at)\\s*((due by)|due|by|from|on|at)", "");
 		input = input.replaceAll("^\\s+|\\s+$", "");
 		return input.trim();
 	}
@@ -93,23 +102,25 @@ public class MainParser {
 
 		if (!parsedInput.isEmpty()) {
 			switch (parsedInput.get(0).getDates().size()) {
-			case 1:
-				if (input.contains("by") || input.contains("due") || input.contains("before")) {
-					// Deadline Task
-					tasks.add(new DeadlineTask(parsedInput.get(0).getDates().get(0), getTaskDesc(input), false));
-					return new ParsedObject(COMMAND_TYPE.ADD, TASK_TYPE.DEADLINE_TASK, tasks);
-				} else {
-					// Single Date Event
-					tasks.add(new Event(parsedInput.get(0).getDates().get(0), parsedInput.get(0).getDates().get(0), getTaskDesc(input), false));
-					return new ParsedObject(COMMAND_TYPE.ADD, TASK_TYPE.SINGLE_DATE_EVENT, tasks);
-				}
-			case 2:
-				// Double Date Event
-				tasks.add(new Event(parsedInput.get(0).getDates().get(0), parsedInput.get(0).getDates().get(1), getTaskDesc(input), false));
-				return new ParsedObject(COMMAND_TYPE.ADD, TASK_TYPE.DOUBLE_DATE_EVENT, tasks);
-			default:
-				// Invalid
-				return new ParsedObject(COMMAND_TYPE.INVALID, null, null);
+				case 1:
+					if (input.contains("by") || input.contains("due") || input.contains("before")) {
+						// Deadline Task
+						parsedInput.get(0).getDates().get(0).setHours(23);
+						parsedInput.get(0).getDates().get(0).setMinutes(59);
+						tasks.add(new DeadlineTask(parsedInput.get(0).getDates().get(0), getTaskDesc(input), false));
+						return new ParsedObject(COMMAND_TYPE.ADD, TASK_TYPE.DEADLINE_TASK, tasks);
+					} else {
+						// Single Date Event
+						tasks.add(new Event(parsedInput.get(0).getDates().get(0), parsedInput.get(0).getDates().get(0), getTaskDesc(input), false));
+						return new ParsedObject(COMMAND_TYPE.ADD, TASK_TYPE.SINGLE_DATE_EVENT, tasks);
+					}
+				case 2:
+					// Double Date Event
+					tasks.add(new Event(parsedInput.get(0).getDates().get(0), parsedInput.get(0).getDates().get(1), getTaskDesc(input), false));
+					return new ParsedObject(COMMAND_TYPE.ADD, TASK_TYPE.DOUBLE_DATE_EVENT, tasks);
+				default:
+					// Invalid
+					return new ParsedObject(COMMAND_TYPE.INVALID, null, null);
 			}
 		} else {
 			// Floating Task
@@ -118,28 +129,45 @@ public class MainParser {
 		}
 	}
 
-	public String[] getCommandParameters(String input, COMMAND_TYPE cmdType) {
+	public ArrayList<String> getCommandParameters(String input, COMMAND_TYPE cmdType) {
 		String pattern;
 		if (cmdType == COMMAND_TYPE.SEARCH || cmdType == COMMAND_TYPE.DISPLAY) {
 			pattern = "\\.+|,+|:+|;+|/+|\\\\+|\\|+";
 		} else {
-			pattern = "\\s+|\\.+|,+|:+|;+|/+|\\\\+|\\|+";
+			if (input.contains("to") || input.contains("-")) {
+				pattern = "\\-+|to";
+			} else {
+				pattern = "\\s+|\\.+|,+|:+|;+|/+|\\\\+|\\|+";
+			}
 		}
 
 		String[] paramArray = input.split(pattern);
+
 		for (int i = 0; i < paramArray.length; i++) {
 			paramArray[i] = paramArray[i].replaceAll("^\\s+|\\s+$", "");
 		}
 
-		return paramArray;
+		ArrayList<String> paramList = new ArrayList<String>(Arrays.asList(paramArray));
+
+		if (input.contains("to") || input.contains("-")) {
+			int fromID = Integer.parseInt(paramList.get(0));
+			int toID = Integer.parseInt(paramList.get(1));
+
+			while (toID-1 >= fromID+1) {
+				paramList.add(1, toID-1 + "");
+				toID--;
+			}
+		}
+
+		return paramList;
 	}
 
 	public ParsedObject getDeleteParsedObject(String input) {
 		String params = removeCommandWord(input, deleteCmdList);
 
 		ArrayList<Integer> taskIDs = new ArrayList<Integer>();
-		String[] taskIDArray = getCommandParameters(params, COMMAND_TYPE.DELETE);
-		for (String idStr : taskIDArray) {
+		ArrayList<String> taskIDList = getCommandParameters(params, COMMAND_TYPE.DELETE);
+		for (String idStr : taskIDList) {
 			if (!idStr.trim().isEmpty()) {
 				try {
 					int taskID = Integer.parseInt(idStr);
@@ -147,6 +175,7 @@ public class MainParser {
 				} catch (NumberFormatException e) {
 					System.out.println("Error");
 					// Catch exception and continue parsing the rest
+					logger.log(Level.SEVERE, e.toString(), e);
 				}
 			}
 		}
@@ -158,7 +187,7 @@ public class MainParser {
 		String params = removeCommandWord(input, displayCmdList);
 
 		ArrayList<String> columns = new ArrayList<String>();
-		String[] colArray = getCommandParameters(params, COMMAND_TYPE.DISPLAY);
+		ArrayList<String> colArray = getCommandParameters(params, COMMAND_TYPE.DISPLAY);
 		for (String colName : colArray) {
 			if (!colName.trim().isEmpty()) {
 				columns.add(colName);
@@ -172,7 +201,7 @@ public class MainParser {
 		String params = removeCommandWord(input, searchCmdList);
 
 		ArrayList<String> searchTerms = new ArrayList<String>();
-		String[] termArray = getCommandParameters(params, COMMAND_TYPE.SEARCH);
+		ArrayList<String> termArray = getCommandParameters(params, COMMAND_TYPE.SEARCH);
 		for (String term : termArray) {
 			if (!term.trim().isEmpty()) {
 				searchTerms.add(term);
@@ -186,13 +215,13 @@ public class MainParser {
 		String params = removeCommandWord(input, searchCmdList);
 
 		ArrayList<String> searchTerms = new ArrayList<String>();
-		String[] termArray = getCommandParameters(params, COMMAND_TYPE.SEARCH);
+		ArrayList<String> termArray = getCommandParameters(params, COMMAND_TYPE.UPDATE);
 		for (String term : termArray) {
 			if (!term.trim().isEmpty()) {
 				searchTerms.add(term);
 			}
 		}
 
-		return new ParsedObject(COMMAND_TYPE.SEARCH, null, searchTerms);
+		return new ParsedObject(COMMAND_TYPE.UPDATE, null, searchTerms);
 	}
 }
