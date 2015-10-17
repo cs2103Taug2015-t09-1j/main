c8c8e37e71fb7ee315ecc70e5e25d6e54de0de03
dyanm <daltonyan@gmail.com>
2015-10-17 19:54:57 +0800

    Created a custom cell editor class to manage the editing of Date objects and updated the custom cell renderer

---
+++ src/ui/CustomCellRenderer.java
@@ -0,0 +1,69 @@
+/**
+ *
+ */
+package ui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Insets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ * @author Dalton
+ *
+ */
+public class CustomCellRenderer extends JTextArea implements TableCellRenderer {
+
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+    	this.setWrapStyleWord(true);
+        this.setLineWrap(true);
+        this.setMargin(new Insets(2,10,2,10));
+        this.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 12));
+
+        if (isSelected) {
+            setBackground(table.getSelectionBackground());
+            setForeground(table.getSelectionForeground());
+        } else {
+        	setBackground(table.getBackground());
+            setForeground(table.getForeground());
+        }
+
+        if (value != null) {
+	        if (value instanceof Date) {
+	        	this.setText(formatDate((Date)value));
+        		long currentTime = System.currentTimeMillis();
+        		long dateTime = ((Date)value).getTime();
+        		if (dateTime < currentTime) {
+	        		this.setForeground(Color.RED);
+	        	} else {
+	        		this.setForeground(Color.decode("0x009900"));
+	        	}
+        		if (table.getName().equals("Events")) {
+	        		long fromDate = ((Date)table.getModel().getValueAt(row, 1)).getTime();
+	        		long toDate = ((Date)table.getModel().getValueAt(row, 2)).getTime();
+	        		if (fromDate == toDate) {
+	        			if (column == 2) {
+	        				this.setText(null);
+	        			}
+	        		}
+        		}
+	        } else {
+        		this.setText(value.toString());
+	        }
+        }
+
+        return this;
+    }
+
+    private String formatDate(Date d) {
+    	SimpleDateFormat dateFmt = new SimpleDateFormat("EEE, dd MMM yyyy");
+		SimpleDateFormat dayFmt = new SimpleDateFormat("h:mm a");
+		return dateFmt.format(d) + "\n" + dayFmt.format(d);
+    }
+}
