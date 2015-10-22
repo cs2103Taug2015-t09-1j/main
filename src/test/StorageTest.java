2a612ce611275b22f828c1d2bc9a392e20d19984
buidohiep <buidohiep@gmail.com>
2015-10-22 09:40:09 +0800

    add some methods to StorageTest.java

--- src/test/StorageTest.java
+++ src/test/StorageTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static models.EnumTypes.TASK_TYPE.*;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.text.DateFormat;
@@ -9,10 +10,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
+import org.ocpsoft.prettytime.shade.net.fortuna.ical4j.model.parameter.Dir;
 
 import models.Event;
 import models.Task;
 import storage.Storage;
+import sun.awt.RepaintArea;
 import storage.DirectoryHandler;
 import storage.FileHandler;
 
@@ -35,11 +38,28 @@ public class StorageTest {
 	@Test 
 	public void testStorageInit() {
 		String workingDir = System.getProperty("user.dir");
-		System.out.println("Current working directory : " + workingDir);
+		workingDir = DirectoryHandler.fixDir(workingDir);
 
 		Storage storage = Storage.getInstance();
 		storage.init();
 		
+		assertEquals(workingDir, FileHandler.readFromFile("config.txt"));
+	}
+	
+	@Test 
+	public void testFileHandler() {
+		String content = "Some content";
+		FileHandler.writeToFile("tem.txt", content);
+		String readBackContent = FileHandler.readFromFile("tem.txt");
+		assertEquals(content, readBackContent);
+	}
+	
+	@Test 
+	public void testChangeDir() {
+		Storage storage = Storage.getInstance();
+		String newDir = "F:\\TEM";
+		storage.setStoreDir(newDir);
+		assertEquals(DirectoryHandler.fixDir(newDir), FileHandler.readFromFile("config.txt"));
 	}
 
 }
